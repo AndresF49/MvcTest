d7b6c7837bb4fd1aff743aaa54f1d643b4d06bce
Andres <andres06037@gmail.com>
2023-02-11 15:20:35 -0600

    stopped at Passing Data from the Controller to the View

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -64,6 +64,7 @@ public class HelloWorldController : Controller
     {
         return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
     }
+    // stopped at https://learn.microsoft.com/en-us/aspnet/core/tutorials/first-mvc-app/adding-view?view=aspnetcore-7.0&tabs=visual-studio-code#passing-data-from-the-controller-to-the-view
 }
 
 
